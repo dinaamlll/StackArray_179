6debdcbf020aed8b7c36f840a24c8500f39f172d
dinaamlll <dinamaliaa17@gmail.com>
2023-06-16 10:56:30 +0700

    void pop

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -24,7 +24,19 @@ public:
 		stack_array[top] = element; //step 3
 		cout << endl;
 		cout << element << "ditambahkan(pushed)" << endl;
-
 		return element;
+	}
+
+	void pop() {
+		if (empty()) {// step 1
+			cout << "\nStack is empty. Cannot pop. " << endl; //1.a
+			return; //1.b
+		}
+
+
+		cout << "\nThe popped element is:" << stack_array[top] << endl; //step 2
+		top--;  //step 3 decrement
+
+
 	}
 };
